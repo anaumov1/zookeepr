611da56de0bcc2d62a5e8b8bd8c6deefc4727ff7
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-07-13 18:51:55 -0500

    initial file structure

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+// instantiate the server
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// getting server to listen
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
