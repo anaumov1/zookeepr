6f901e8cd64a0a876828b76c5e90efa7ee7179c3
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-07-14 00:12:12 -0500

    added PORT to run

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+// code to let heroku set an environment so it can run on port 80
+const PORT = process.env.PORT || 3001;
+
 // instantiate the server
 const app = express();
 
@@ -65,8 +68,7 @@ app.get('/api/animals', (req, res) => {
 
 
 // getting server to listen
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
 
-  
