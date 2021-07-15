63cc40db98f4ed87e941be9b72623216388c8543
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-07-15 13:36:53 -0500

    added route from server to animals.html

--- server.js
+++ server.js
@@ -134,6 +134,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
 
 
 // getting server to listen
