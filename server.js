c933932204f21727622eef54ce9f345d8238fc23
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-07-15 16:43:40 -0500

    modularized server.js

--- server.js
+++ server.js
@@ -1,44 +1,19 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
 
-// code to let heroku set an environment so it can run on port 80
 const PORT = process.env.PORT || 3001;
-
-// instantiate the server
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
-// route to add css and js
 app.use(express.static('public'));
 
-// require data
-const { animals } = require('./data/animals');
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/animals', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-app.get('/zookeepers', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-
-
-// getting server to listen
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
+  console.log(`API server now on port ${PORT}!`);
+});
 
