e120ea029cdc4c88d5d2b15a6235ce1b71ac2ca5
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-07-15 16:11:57 -0500

    completed animalRoutes.js

--- routes/apiRoutes/animalRoutes.js
+++ routes/apiRoutes/animalRoutes.js
@@ -4,7 +4,7 @@ const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('..
 const { animals } = require('../../data/animals');
 
  // adding the route
- app.get('/animals', (req, res) => {
+ router.get('/animals', (req, res) => {
     let results =animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
@@ -14,7 +14,7 @@ const { animals } = require('../../data/animals');
 
 
 // creating a new GET route for animals using animal id to sort by animal
-app.get('/animals/:id',  (req, res) => {
+router.get('/animals/:id',  (req, res) => {
     const result = findById(req.params.id, animals);
     if (result) {
       res.json(result); 
@@ -26,7 +26,7 @@ app.get('/animals/:id',  (req, res) => {
  
 
 // create a route to the server that accepts data to be used or stored in server
-app.post('/animals', (req,res) => {
+router.post('/animals', (req,res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
   
@@ -38,3 +38,5 @@ app.post('/animals', (req,res) => {
       res.json(animal);
     }
   });
+
+  module.exports  = router;
